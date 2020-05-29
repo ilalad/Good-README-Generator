b775e9bb90d8ca7790191bfdcb69a98373611703
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-05-28 20:37:25 -0500

    array of questions and variables

---
+++ Develop/index.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+const generateMarkdown = require("./generateMarkdown.js");
+
+//array of questions 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your GitHub username",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub email",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "The URL to your project",
+        name: "URL",
+    },
+    {
+        type: "input",
+        message: "What is the project name",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message: "Write a description of your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Table of Contents",
+        name: "tableofcontents",
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have (user arrow keys to select",
+        name: "license",
+        choices: ["MIT", "APACHE", "GPL", "BSD", "None"],
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo",
+        name: "contributors",
+    }, {
+        type: "input",
+        message: "What command should be run to run tests",
+        name: "tests",
+    }, {
+        type: "input",
+        message: "Questions",
+        name: "questions",
+    },
+])
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Good-README-Generator
 Application to dynamically generate a README.md from user's input.
+
+
+* At least one badge
+* Project title
+* Description
+* Table of Contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+  * User GitHub profile picture
+  * User GitHub email
